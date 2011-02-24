aa1b1e8e9faf580f98dea61fed410e48f8cf4964
Michael Schurter <michael@susens-schurter.com>
2011-02-23 22:20:55 -0800

    Switch to a simpler namedtuples like class for SigInfo

--- signalfd/__init__.py
+++ signalfd/__init__.py
@@ -1,7 +1,10 @@
 # Copyright (c) 2010 Jean-Paul Calderone.
 # See LICENSE file for details.
 import os
+from operator import itemgetter
 import struct
+
+SIGINFO_SZ = 128
 
 try:
     from signalfd._signalfd import \
@@ -28,42 +31,6 @@ __all__ = [
 
     '__version__', '__version_info__']
 
-_signo_to_str = {
-        1: 'SIGHUP',
-        2: 'SIGINT',
-        3: 'SIGQUIT',
-        4: 'SIGILL',
-        5: 'SIGTRAP',
-        6: 'SIGABRT',
-        7: 'SIGBUS',
-        8: 'SIGFPE',
-        9: 'SIGKILL',
-        10: 'SIGUSR1',
-        11: 'SIGSEGV',
-        12: 'SIGUSR2',
-        13: 'SIGPIPE',
-        14: 'SIGALRM',
-        15: 'SIGTERM',
-        16: 'SIGSTKFLT',
-        17: 'SIGCHLD',
-        18: 'SIGCONT',
-        19: 'SIGSTOP',
-        20: 'SIGTSTP',
-        21: 'SIGTTIN',
-        22: 'SIGTTOU',
-        23: 'SIGURG',
-        24: 'SIGXCPU',
-        25: 'SIGXFSZ',
-        26: 'SIGVTALRM',
-        27: 'SIGPROF',
-        28: 'SIGWINCH',
-        29: 'SIGIO',
-        30: 'SIGPWR',
-        31: 'SIGSYS',
-        34: 'SIGRTMIN',
-        64: 'SIGRTMAX',
-    }
-
 def create_signalfd(signals, flags=0):
     """Creates a new signal file descriptor
 
@@ -76,40 +43,85 @@ def create_signalfd(signals, flags=0):
 
 def read_signalfd(fd):
     """Given a signalfd, return a SigInfo instance"""
-    return SigInfo(os.read(fd, siginfo_t.size))
+    return SigInfo(*struct.unpack(siginfo_t, os.read(fd, SIGINFO_SZ)))
 
-class SigInfo(object):
-    def __init__(self, data):
-        (self.ssi_signo,
-        self.ssi_errno,
-        self.ssi_code,
-        self.ssi_pid,
-        self.ssi_uid,
-        self.ssi_fd,
-        self.ssi_tid,
-        self.ssi_band,
-        self.ssi_overrun,
-        self.ssi_trapno,
-        self.ssi_status,
-        self.ssi_int,
-        self.ssi_ptr,
-        self.ssi_utime,
-        self.ssi_stime,
-        self.ssi_addr,
-        self.padding) = siginfo_t.unpack(data)
+class SigInfo(tuple):
+        """SigInfo(*fields)
 
-    @property
-    def signo(self):
-        return self.ssi_signo
+        Class representing signal information
+        """
 
-    @property
-    def signal(self):
-        return _signo_to_str[self.ssi_signo]
+        __slots__ = ()
 
-    def __str__(self):
-        return self.signal
+        _fields = ('ssi_signo', 'ssi_errno', 'ssi_code', 'ssi_pid', 'ssi_uid',
+                'ssi_fd', 'ssi_tid', 'ssi_band', 'ssi_overrun', 'ssi_trapno',
+                'ssi_status', 'ssi_int', 'ssi_ptr', 'ssi_utime', 'ssi_stime',
+                'ssi_addr', 'padding')
 
-siginfo_t = struct.Struct(
+        def __new__(cls, ssi_signo, ssi_errno, ssi_code, ssi_pid, ssi_uid,
+                ssi_fd, ssi_tid, ssi_band, ssi_overrun, ssi_trapno, ssi_status,
+                ssi_int, ssi_ptr, ssi_utime, ssi_stime, ssi_addr, padding):
+            return tuple.__new__(cls, (ssi_signo, ssi_errno, ssi_code, ssi_pid,
+                ssi_uid, ssi_fd, ssi_tid, ssi_band, ssi_overrun, ssi_trapno,
+                ssi_status, ssi_int, ssi_ptr, ssi_utime, ssi_stime, ssi_addr,
+                padding))
+
+        @classmethod
+        def _make(cls, iterable, new=tuple.__new__, len=len):
+            """Make a new SigInfo object from a sequence or iterable"""
+            result = new(cls, iterable)
+            if len(result) != 17:
+                raise TypeError('Expected 17 arguments, got %d' % len(result))
+            return result
+
+        def __str__(self):
+            return '<SigInfo %d>' % self.ssi_signo
+
+        def __repr__(self):
+            return 'SigInfo(ssi_signo=%r, ssi_errno=%r, ssi_code=%r, ssi_pid=%r, ssi_uid=%r, ssi_fd=%r, ssi_tid=%r, ssi_band=%r, ssi_overrun=%r, ssi_trapno=%r, ssi_status=%r, ssi_int=%r, ssi_ptr=%r, ssi_utime=%r, ssi_stime=%r, ssi_addr=%r, padding=%r)' % self
+
+        def _asdict(t):
+            """Return a new dict which maps field names to their values"""
+            return {'ssi_signo': t[0], 'ssi_errno': t[1], 'ssi_code': t[2], 'ssi_pid': t[3], 'ssi_uid': t[4], 'ssi_fd': t[5], 'ssi_tid': t[6], 'ssi_band': t[7], 'ssi_overrun': t[8], 'ssi_trapno': t[9], 'ssi_status': t[10], 'ssi_int': t[11], 'ssi_ptr': t[12], 'ssi_utime': t[13], 'ssi_stime': t[14], 'ssi_addr': t[15], 'padding': t[16]}
+
+        def _replace(self, **kwds):
+            'Return a new SigInfo object replacing specified fields with new values'
+            result = self._make(map(kwds.pop, ('ssi_signo', 'ssi_errno',
+                'ssi_code', 'ssi_pid', 'ssi_uid', 'ssi_fd', 'ssi_tid',
+                'ssi_band', 'ssi_overrun', 'ssi_trapno', 'ssi_status',
+                'ssi_int', 'ssi_ptr', 'ssi_utime', 'ssi_stime', 'ssi_addr',
+                'padding'), self))
+            if kwds:
+                raise ValueError('Got unexpected field names: %r' % kwds.keys())
+            return result
+
+        def __getnewargs__(self):
+            return tuple(self)
+
+        ssi_signo = property(itemgetter(0), doc='Signal number')
+        ssi_errno = property(itemgetter(1), doc='Error number (unused)')
+        ssi_code = property(itemgetter(2),  doc='Signal code')
+        ssi_pid = property(itemgetter(3),   doc='PID of sender')
+        ssi_uid = property(itemgetter(4),   doc='Real UID of sender')
+        ssi_fd = property(itemgetter(5),    doc='File descriptor (SIGIO)')
+        ssi_tid = property(itemgetter(6),   doc='Kernel timer ID')
+        ssi_band = property(itemgetter(7),  doc='Band event (SIGIO)')
+        ssi_overrun = property(itemgetter(8), doc='POSIX timer overrun count')
+        ssi_trapno = property(itemgetter(9),  doc='Trap number')
+        ssi_status = property(itemgetter(10),
+                doc='Exit status or signal (SIGCHLD)')
+        ssi_int = property(itemgetter(11),  doc='Integer sent by sigqueue')
+        ssi_ptr = property(itemgetter(12),  doc='Pointer sent by sigqueue')
+        ssi_utime = property(itemgetter(13),
+                doc='User CPU time consumed (SIGCHLD)')
+        ssi_stime = property(itemgetter(14),
+                doc='System CPU time consumed (SIGCHLD)')
+        ssi_addr = property(itemgetter(15), doc='Address that generated '
+                'signal (for hardware generated signals)')
+        padding = property(itemgetter(16),  doc='Pad size to 128 bytes')
+
+
+siginfo_t = (
         'I' # ssi_signo   - Signal number
         'i' # ssi_errno   - Error number (unused)
         'i' # ssi_code    - Signal code
